76d76e3ec503a4a11090574c7ac30e9b9ec57709
Anmoljot Kaur <127451556+anmoljot-kaur@users.noreply.github.com>
2023-07-24 15:25:07 -0400

    Update and rename Amazon RDS to Amazon RDS.bash

---
+++ Amazon RDS.bash
@@ -0,0 +1,14 @@
+# Migrating to Amazon RDS
+# Author: Anmoljot Kaur
+# Date: July 24, 2023
+# Objectives:
+#   1. Create an Amazon RDS MariaDB instance by using the AWS CLI.
+#   2. Migrate data from a MariaDB database on an EC2 instance to an Amazon RDS MariaDB instance.
+#   3. Monitor the Amazon RDS instance by using Amazon CloudWatch metrics.
+
+# In this project, you migrate the café web application to use a fully managed Amazon Relational Database Service (Amazon RDS) database (DB) instance instead of a local database instance.
+
+#Connect to AWS CLI via putty
+
+# Step 1: Configure the AWS CLI
+aws configure
